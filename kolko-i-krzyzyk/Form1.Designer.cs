1b400ebdf0e04395da488400e026ff80edb8f562
Piotr Bujnowski <bujan98pl@gmail.com>
2021-06-15 14:05:26 +0200

    Project init

---
+++ kolko-i-krzyzyk/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace kolko_i_krzyzyk
+{
+    partial class kolko_i_krzyzyk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // kolko_i_krzyzyk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 429);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "kolko_i_krzyzyk";
+            this.Text = "Kółko i Krzyżyk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ kolko-i-krzyzyk/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kolko_i_krzyzyk
+{
+    public partial class kolko_i_krzyzyk : Form
+    {
+        public kolko_i_krzyzyk()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
