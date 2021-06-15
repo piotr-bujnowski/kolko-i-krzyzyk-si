fffce964607b8d5f524ee2f681339d86e63ee5a6
Piotr Bujnowski <bujan98pl@gmail.com>
2021-06-15 14:40:27 +0200

    Add X or O on cell click

--- kolko-i-krzyzyk/Kolko-i-Krzyzyk-Form.Designer.cs
+++ kolko-i-krzyzyk/Kolko-i-Krzyzyk-Form.Designer.cs
@@ -49,84 +49,111 @@ namespace kolko_i_krzyzyk
             // 
             // A1
             // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A1.Location = new System.Drawing.Point(12, 18);
             this.A1.Name = "A1";
             this.A1.Size = new System.Drawing.Size(90, 90);
             this.A1.TabIndex = 0;
             this.A1.Text = " ";
             this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Cell_Click);
+            this.A1.TabStop = false;
             // 
             // A2
             // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A2.Location = new System.Drawing.Point(108, 18);
             this.A2.Name = "A2";
             this.A2.Size = new System.Drawing.Size(90, 90);
             this.A2.TabIndex = 1;
             this.A2.Text = " ";
             this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Cell_Click);
+            this.A2.TabStop = false;
             // 
             // A3
             // 
+            this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.A3.Location = new System.Drawing.Point(204, 18);
             this.A3.Name = "A3";
             this.A3.Size = new System.Drawing.Size(90, 90);
             this.A3.TabIndex = 2;
             this.A3.Text = " ";
             this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.Cell_Click);
+            this.A3.TabStop = false;
             // 
             // B1
             // 
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B1.Location = new System.Drawing.Point(12, 114);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(90, 90);
             this.B1.TabIndex = 3;
             this.B1.Text = " ";
             this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Cell_Click);
+            this.B1.TabStop = false;
             // 
             // B2
             // 
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B2.Location = new System.Drawing.Point(108, 114);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(90, 90);
             this.B2.TabIndex = 4;
             this.B2.Text = " ";
             this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Cell_Click);
+            this.B2.TabStop = false;
             // 
             // B3
             // 
+            this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.B3.Location = new System.Drawing.Point(204, 114);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(90, 90);
             this.B3.TabIndex = 5;
             this.B3.Text = " ";
             this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.Cell_Click);
+            this.B3.TabStop = false;
             // 
             // C1
             // 
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C1.Location = new System.Drawing.Point(12, 210);
             this.C1.Name = "C1";
             this.C1.Size = new System.Drawing.Size(90, 90);
             this.C1.TabIndex = 6;
             this.C1.Text = " ";
             this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Cell_Click);
+            this.C1.TabStop = false;
             // 
             // C2
             // 
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C2.Location = new System.Drawing.Point(108, 210);
             this.C2.Name = "C2";
             this.C2.Size = new System.Drawing.Size(90, 90);
             this.C2.TabIndex = 7;
             this.C2.Text = " ";
             this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Cell_Click);
+            this.C2.TabStop = false;
             // 
             // C3
             // 
+            this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.C3.Location = new System.Drawing.Point(204, 210);
             this.C3.Name = "C3";
             this.C3.Size = new System.Drawing.Size(90, 90);
             this.C3.TabIndex = 8;
             this.C3.Text = " ";
             this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.Cell_Click);
+            this.C3.TabStop = false;
             // 
             // cellGrpBox
             // 
--- kolko-i-krzyzyk/Kolko-i-Krzyzyk-Form.cs
+++ kolko-i-krzyzyk/Kolko-i-Krzyzyk-Form.cs
@@ -13,9 +13,35 @@ namespace kolko_i_krzyzyk
 {
     public partial class kolko_i_krzyzyk : Form
     {
+        private Button[,] cellArray;
+        private int clickCounter;
+
         public kolko_i_krzyzyk()
         {
             InitializeComponent();
+
+            cellArray = new Button[3, 3]
+            {
+                { A1, A2, A3 },
+                { B1, B2, B3 },
+                { C1, C2, C3 }
+            };
+        }
+
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            Button cell = (Button)sender;
+
+            if (clickCounter % 2 == 0)
+            {
+                cell.Text = "X";
+            }
+            else
+            {
+                cell.Text = "O";
+            }
+            cell.Enabled = false;
+            clickCounter += 1;
         }
     }
 }
